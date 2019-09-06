bc035b694f0014a59957604293173ad7ff3f6e2c
Riley Niu <hn263@cornell.edu>
2019-09-06 15:02:48 -0400

    starters

---
+++ project1/filters/src/cs4620/filters/NearestNeighborResampler.java
@@ -0,0 +1,17 @@
+package cs4620.filters;
+
+/**
+ * A resampler that uses nearest-neighbor interpolation.
+ * 
+ * @author srm
+ * @author Your Name(s) Here
+ */
+public class NearestNeighborResampler implements ResampleEngine {
+
+    @Override
+    public void resample(SimpleImage src, SimpleImage dst, double left, double bottom, double right, double top) {
+        // TODO nearest-neighbor sampling
+        
+    }
+
+}
