447f538ef335c586889f34db699f4cbc6a7953f0
Riley Niu <hn263@cornell.edu>
2019-10-03 01:16:31 -0400

    triangle intersection done

--- ray/src/ray1/surface/Triangle.java
+++ ray/src/ray1/surface/Triangle.java
@@ -2,6 +2,11 @@ package ray1.surface;
 
 import ray1.IntersectionRecord;
 import ray1.Ray;
+
+import java.util.ArrayList;
+
+import egl.math.Vector2;
+import egl.math.Vector2d;
 import egl.math.Vector3;
 import egl.math.Vector3d;
 import ray1.shader.Shader;
@@ -62,12 +67,64 @@ public class Triangle extends Surface {
    */
   public boolean intersect(IntersectionRecord outRecord, Ray rayIn) {
     // TODO#Ray Task 2: fill in this function.
-
-    
-    // If there was an intersection, fill out the intersection record
-
-
-	return true;
+	  double g = rayIn.direction.x;
+	  double h = rayIn.direction.y;
+	  double i = rayIn.direction.z;
+	  Vector3 v0 = owner.getMesh().getPosition(face,0);
+	  double j = v0.x - rayIn.origin.x;
+	  double k = v0.y - rayIn.origin.y;
+	  double l = v0.z - rayIn.origin.z;
+	  
+	  // in-triangle test by cramer rule
+	  double detA = a*(e*i-f*h) - b*(d*i - f*g) + c*(d*h - e*g);
+	  double t = -(f*(a*k -j*b) + e*(j*c - a*l) + d*(b*l - k*c))/detA;
+	  if ((t<rayIn.start) || (t> rayIn.end)) {
+		  return false;
+	  }
+	  double gamma = (i*(a*k - j*b) + h*(j*c - a*l) + g*(b*l - k*c))/detA;
+	  if ((gamma<0) || (gamma> 1)) {
+		  return false;
+	  }
+	  double beta = (j*(e*i - h*f) + k*(g*f - d*i) + l*(d*h - e*g))/detA;
+	  if ((beta<0) || (beta> 1-gamma)) {
+		  return false;
+	  }
+	    
+	  // If there was an intersection, fill out the intersection record
+	  
+	  // set location
+	  Vector3d pos = new Vector3d();
+      rayIn.evaluate(pos,t);
+      outRecord.location.set(pos);
+      
+      // set normals
+      if(this.face.hasNormals()) {
+    	  Vector3 na = this.owner.getMesh().getNormal(this.face, 0);
+    	  Vector3 nb = this.owner.getMesh().getNormal(this.face, 1);
+    	  Vector3 nc = this.owner.getMesh().getNormal(this.face, 2);
+    	  double alpha = 1-gamma - beta;
+    	  double nx =alpha * na.x + beta * nb.x +gamma* nc.x;
+    	  double ny =alpha * na.y + beta * nb.y +gamma* nc.y;
+    	  double nz =alpha * na.z + beta * nb.z +gamma* nc.z;
+    	  outRecord.normal.set(new Vector3d(nx,ny,nz).normalize());
+    	  
+      }else {
+    	  outRecord.normal.set(this.norm);
+      }
+      
+      // set uvs
+      if (face.hasUVs()) {
+		  Vector2 uv0 = owner.getMesh().getUV(face, 0);
+		  Vector2 uv1 = owner.getMesh().getUV(face, 1);
+		  Vector2 uv2 = owner.getMesh().getUV(face, 2);
+		  Vector2d uv = new Vector2d();
+		  uv.addMultiple((1 - beta - gamma), uv0).addMultiple(beta, uv1).addMultiple(gamma, uv2);
+		  outRecord.texCoords.set(uv);
+	  }
+	  outRecord.t = t;
+	  outRecord.surface = this;
+	  return true;
+	  
   }
 
   /**
--- ray/src/ray1/tests/SceneTests.java
+++ ray/src/ray1/tests/SceneTests.java
@@ -13,46 +13,46 @@ import ray1.surface.Sphere;
 
 public class SceneTests {
 
-//    @Test
-//    public void testAnyIntersection() {
-//        System.out.println("\nTest getAnyIntersection() method.");
-//        System.out.println("==============================================");
-//
-//        // I'm just going to add spheres because they're easier to make.
-//        Sphere sphere0 = new Sphere();
-//        Sphere sphere1 = new Sphere();
-//        sphere1.setCenter(new Vector3(1, 0, 0));
-//
-//        Scene scene = new Scene();
-//        scene.addSurface(sphere0);
-//        scene.addSurface(sphere1);
-//
-//        Vector3d rayOrigin = new Vector3d();
-//        Vector3d rayDirection = new Vector3d();
-//
-//        // Begin tests
-//        rayOrigin.set(3, 0, 0);
-//        rayDirection.set(-1, 0, 0);
-//        Ray ray0 = new Ray(rayOrigin, rayDirection);
-//        ray0.makeOffsetRay();
-//        System.out.println("Ray 0: origin = " + ray0.origin + "; dir = " + ray0.direction);
-//        assertTrue(scene.getAnyIntersection(ray0));
-//
-//        rayOrigin.set(-3, 0, 0);
-//        rayDirection.set(1, 0, 0);
-//        Ray ray1 = new Ray(rayOrigin, rayDirection);
-//        ray1.makeOffsetRay();
-//        System.out.println("Ray 1: origin = " + ray1.origin + "; dir = " + ray1.direction);
-//        assertTrue(scene.getAnyIntersection(ray1));
-//
-//        rayOrigin.set(5, 0, 0);
-//        rayDirection.set(0, 1, 0);
-//        Ray ray2 = new Ray(rayOrigin, rayDirection);
-//        System.out.println("Ray 2: origin = " + ray2.origin + "; dir = " + ray2.direction);
-//        assertTrue(!scene.getAnyIntersection(ray2));
-//
-//        System.out.println("All tests passed.\n");
-//    }
+    @Test
+    public void testAnyIntersection() {
+        System.out.println("\nTest getAnyIntersection() method.");
+        System.out.println("==============================================");
+
+        // I'm just going to add spheres because they're easier to make.
+        Sphere sphere0 = new Sphere();
+        Sphere sphere1 = new Sphere();
+        sphere1.setCenter(new Vector3(1, 0, 0));
+
+        Scene scene = new Scene();
+        scene.addSurface(sphere0);
+        scene.addSurface(sphere1);
+
+        Vector3d rayOrigin = new Vector3d();
+        Vector3d rayDirection = new Vector3d();
+
+        // Begin tests
+        rayOrigin.set(3, 0, 0);
+        rayDirection.set(-1, 0, 0);
+        Ray ray0 = new Ray(rayOrigin, rayDirection);
+        ray0.makeOffsetRay();
+        System.out.println("Ray 0: origin = " + ray0.origin + "; dir = " + ray0.direction);
+        assertTrue(scene.getAnyIntersection(ray0));
+
+        rayOrigin.set(-3, 0, 0);
+        rayDirection.set(1, 0, 0);
+        Ray ray1 = new Ray(rayOrigin, rayDirection);
+        ray1.makeOffsetRay();
+        System.out.println("Ray 1: origin = " + ray1.origin + "; dir = " + ray1.direction);
+        assertTrue(scene.getAnyIntersection(ray1));
+
+        rayOrigin.set(5, 0, 0);
+        rayDirection.set(0, 1, 0);
+        Ray ray2 = new Ray(rayOrigin, rayDirection);
+        System.out.println("Ray 2: origin = " + ray2.origin + "; dir = " + ray2.direction);
+        assertTrue(!scene.getAnyIntersection(ray2));
+
+        System.out.println("All tests passed.\n");
+    }
 
     @Test
     public void testFirstIntersection() {
